1e8fa36f4cc3182d252841b2e4b8491e34b91a97
Diem Lam <dlam@Diems-MacBook-Pro.local>
2017-03-05 14:25:20 -0500

    Adding examples for Navigator

---
+++ examples/TestRoute.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+import {
+   StyleSheet,
+   Text,
+   Navigator,
+   TouchableOpacity
+} from 'react-native'
+
+import Index from './Index'
+import Login from './Login'
+
+export default class TestRoute extends Component {
+    render() {
+        return (
+            <Navigator
+                initialRoute = {{ title: 'Index', name: 'Index' }}
+                renderScene = { this.renderScene }
+            />
+        );
+    }
+
+    renderScene(route, navigator) {
+        if (route.name == 'Index') {
+            return (
+                <Index
+                    navigator = { navigator }
+                    {...route.passProps}
+                />
+            )
+        }
+        if (route.name == 'Login') {
+            console.log("in Router " + route.username);
+            return (
+                <Login
+                    navigator = { navigator }
+                    username = { route.username }
+                />
+            )
+        }
+    }
+}
